496631228d646c7712907fd3a6a57e46e06811d5
Martin Stein <mastein@paloaltonetworks.com>
2020-05-11 21:28:34 -0700

    Dockerize

--- src/main/sh/jacoco-upload.sh
+++ src/main/sh/jacoco-upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-FILE=$1
-RUN_NAME=$2
-SERVER=$3
-curl -F "file=@${FILE}" -F "runName=${RUN_NAME}" -H 'enctype: multipart/form-data' http://${SERVER}/jacocofile
+FILE=${1:-src/test/resources/sample.xml}
+RUN_NAME=${2:-"$(date)"}
+SERVER=${3:-localhost:8080}
+echo "Uploading $FILE to $SERVER for run $RUN_NAME"
+curl -F "file=@${FILE};type=text/xml" -F "runName=${RUN_NAME}" -H 'enctype: multipart/form-data' http://${SERVER}/jacocofile
